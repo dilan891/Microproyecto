3a3b112849e37f506c73d3d08bc67efba67e2ed8
Sandra Sánchez <sansanvirom@gmail.com>
2023-03-11 15:28:08 -0400

    “cardMovie”

--- src/components/MovieCard/Moviecard.jsx
+++ src/components/MovieCard/Moviecard.jsx
@@ -1,8 +1,20 @@
+import styled from "./MovieCard.module.css";
+import React from 'react';
 
 export function MovieCard() {
     return(
-        <>
-            carta de pelicula 
-        </>
+        <form>
+
+<div class={styled.margin}></div>
+<div class={styled.card}>
+<img class={styled.imagen} src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/6jOpyXVzQyYL4QB12VRpHUxdwg1.jpg"/>
+  <div class={styled.container2}>
+  <h2 class={styled.title}>Título</h2>
+  <p class={styled.date}>11/03/2023</p>
+  </div>
+</div>
+
+    </form>
     )
 }
+
